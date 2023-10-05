e8b8ece46b5750fe072d8a7a21aed6126248b36f
Julien <parsy.julien@hotmail.fr>
2023-10-05 16:46:18 +0200

    Add env.local config & create User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,41 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
 class AppFixtures extends Fixture
 {
+    private $hasher;
+    private $manager;
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->manager = $manager;
+        $this->loadAdmins();
+    }
+    public function loadAdmins(): void
+    {
+        $datas = [
+            [
+                'email' => 'admin@example.com',
+                'password' => '123',
+                'roles' => ['ROLE_ADMIN'],
+                'enabled' => true,
+            ],
+        ];
 
-        $manager->flush();
+        foreach ($datas as $data) {
+            $user = new User();
+            $user->setEmail($data['email']);
+            $password = $this->hasher->hashPassword($user, $data['password']);
+            $user->setPassword($password);
+            $user->setRoles($data['roles']);
+
+            $this->manager->persist($user);
+        }
+
+        $this->manager->flush();
     }
 }
