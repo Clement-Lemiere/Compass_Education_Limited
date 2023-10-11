8362f6c8868115cd84cb41a6aebd1828e828075d
Julien <parsy.julien@hotmail.fr>
2023-10-11 16:12:30 +0200

    Add Lesson.php and LessonRepository.php in src folder

---
+++ src/Entity/Lesson.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LessonRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LessonRepository::class)]
+class Lesson
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $exercices = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getExercices(): ?string
+    {
+        return $this->exercices;
+    }
+
+    public function setExercices(?string $exercices): static
+    {
+        $this->exercices = $exercices;
+
+        return $this;
+    }
+}
