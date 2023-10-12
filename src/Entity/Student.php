3894d8b0a4d317de3884c965acbed9ce56dc34c6
Clement_Lemière <clement.lemiere.cl@gmail.com>
2023-10-12 07:31:48 +0200

    Create Student entity and student repository

---
+++ src/Entity/Student.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+class Student
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 191)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 191)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $birthdate = null;
+
+    #[ORM\Column(length: 191)]
+    private ?string $nationality = null;
+
+    #[ORM\Column(length: 191, nullable: true)]
+    private ?string $grades = null;
+
+    #[ORM\Column]
+    private ?int $level = null;
+
+    #[ORM\Column(length: 191, nullable: true)]
+    private ?string $courses = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(\DateTimeInterface $birthdate): static
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    public function getNationality(): ?string
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(string $nationality): static
+    {
+        $this->nationality = $nationality;
+
+        return $this;
+    }
+
+    public function getGrades(): ?string
+    {
+        return $this->grades;
+    }
+
+    public function setGrades(?string $grades): static
+    {
+        $this->grades = $grades;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): static
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getCourses(): ?string
+    {
+        return $this->courses;
+    }
+
+    public function setCourses(?string $courses): static
+    {
+        $this->courses = $courses;
+
+        return $this;
+    }
+}
