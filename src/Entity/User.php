2887c0ac39b09891694fed8f05abbe1fddc03046
Clement_Lemière <clement.lemiere.cl@gmail.com>
2023-10-24 11:08:36 +0200

    User testController

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
-    private ?string $email = null;
+    private ?email $email = null;
 
     #[ORM\Column]
     private array $roles = [];
@@ -132,4 +132,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    // public function __toString(){
+    //     return $this->email;
+    // }
 }
