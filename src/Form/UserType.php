ab670f0f87b3ce0ce788e81e5ed5020f285fccf2
Julien <parsy.julien@hotmail.fr>
2023-10-31 09:56:03 +0100

    Update Entity/user add more role & update templates user, login, & Form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,15 +6,43 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
 
 class UserType extends AbstractType
 {
+    private $hasher;
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        
         $builder
             ->add('email')
-            // ->add('roles')
-            // ->add('password')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_STUDENT' => 'student',
+                    'ROLE_TEACHER' => 'teacher',
+                    'ROLE_ADMIN' => 'admin',
+                ],
+                'multiple' => true, // Permet la sélection multiple si nécessaire
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => ['attr' => [
+                    'class' => 'password-field',
+                    'autocomplete' => 'new-password'
+                ]],
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat your password'],
+                'required' => true,
+            ])
             // ->add('student')
             // ->add('teacher')
         ;
