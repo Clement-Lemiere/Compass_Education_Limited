1af547d73d7606635eac5585d5b9107d1b5e4be7
Julien <parsy.julien@hotmail.fr>
2023-11-08 13:51:30 +0100

    Create new templates /front(contact, faq, languages, tips) Update reactController AboutUs, FrontNav & FrontController Update app.scss

--- assets/react/controllers/AboutUs.jsx
+++ assets/react/controllers/AboutUs.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 
+const Test = require('../../images/test.png');
+
 const AboutUs = () => {
 
     return (
 
-        <section className="" id="">
-            <div className="aboutUsCtn">
-                <h2>About us</h2>
-                <div className="mainctn">
-                    <div className="aboutUsblck">
-                        <p className="aboutUsPr"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti adipisci illo at ut iure, ducimus eum! Exercitationem autem animi cum adipisci nobis quae culpa! Facilis nisi, officia officiis dignissimos quia rerum architecto quam porro deleniti distinctio repudiandae placeat, adipisci ea molestias assumenda quidem reprehenderit minus eaque ducimus maxime tempora excepturi! Locuteurs natifs Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, est. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam delectus, voluptas blanditiis inventore tempore nisi obcaecati, optio cum sapiente a, nemo officia dolor veritatis. Recusandae, incidunt adipisci ipsum voluptate rerum iure consequuntur quo minus! Ex beatae iure ducimus illo dolore odio aperiam, fugiat nesciunt, ratione non libero provident ipsa esse.</p>
-                    </div>
-                    <div className="aboutUsImg">
-                        <img src="assets/images/aboutUs.png" alt="About us" />
-                    </div>
+        <section className="aboutUsMain" id="">
+            <h2>About us</h2>
+            <div>
+                <h3>Leyli</h3>
+            </div>
+            <div className="aboutUsBlock">
+                <p className="aboutUsPr"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti adipisci illo at ut iure, ducimus eum! Exercitationem autem animi cum adipisci nobis quae culpa! Facilis nisi, officia officiis dignissimos quia rerum architecto quam porro deleniti distinctio repudiandae placeat, adipisci ea molestias assumenda quidem reprehenderit minus eaque ducimus maxime tempora excepturi! Locuteurs natifs Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, est. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam delectus, voluptas blanditiis inventore tempore nisi obcaecati, optio cum sapiente a, nemo officia dolor veritatis. Recusandae, incidunt adipisci ipsum voluptate rerum iure consequuntur quo minus! Ex beatae iure ducimus illo dolore odio aperiam, fugiat nesciunt, ratione non libero provident ipsa esse.
+                </p>
+                <div className="aboutUsImg">
+                    <img
+                        src={ Test }
+                        alt="About us"
+                    />
                 </div>
-                <div className="mainctn">
-                    <div className="aboutUsblck">
-                        <p className="aboutUsPr"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti adipisci illo at ut iure, ducimus eum! Exercitationem autem animi cum adipisci nobis quae culpa! Facilis nisi, officia officiis dignissimos quia rerum architecto quam porro deleniti distinctio repudiandae placeat, adipisci ea molestias assumenda quidem reprehenderit minus eaque ducimus maxime tempora excepturi! Locuteurs natifs Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, est. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam delectus, voluptas blanditiis inventore tempore nisi obcaecati, optio cum sapiente a, nemo officia dolor veritatis. Recusandae, incidunt adipisci ipsum voluptate rerum iure consequuntur quo minus! Ex beatae iure ducimus illo dolore odio aperiam, fugiat nesciunt, ratione non libero provident ipsa esse.</p>
-                    </div>
-                    <div className="aboutUsImg"> 
-                        <img src="assets/images/aboutUs.png" alt="About us" />
-                    </div>
+            </div>
+            <div>
+                <h3>Chris</h3>
+            </div>
+            <div className="aboutUsBlock">
+                <div className="aboutUsImg">
+                    <img
+                        src={ Test }
+                        alt="About us"
+                    />
                 </div>
-                <div className="discoverBtn">
-                    <a href="#">About us</a>
-                </div>
+                <p className="aboutUsPr"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti adipisci illo at ut iure, ducimus eum! Exercitationem autem animi cum adipisci nobis quae culpa! Facilis nisi, officia officiis dignissimos quia rerum architecto quam porro deleniti distinctio repudiandae placeat, adipisci ea molestias assumenda quidem reprehenderit minus eaque ducimus maxime tempora excepturi! Locuteurs natifs Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, est. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam delectus, voluptas blanditiis inventore tempore nisi obcaecati, optio cum sapiente a, nemo officia dolor veritatis. Recusandae, incidunt adipisci ipsum voluptate rerum iure consequuntur quo minus! Ex beatae iure ducimus illo dolore odio aperiam, fugiat nesciunt, ratione non libero provident ipsa esse.
+                </p>
+            </div>
+            <div className="discoverBtn">
+                <a href="#">About us</a>
             </div>
         </section>
 
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +9,52 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/')]
 class FrontController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/home', name: 'home')]
     public function home(): Response
     {
         
         return $this->render('front/home.html.twig');
     }
 
+    #[Route('/languages', name: 'languages')]
+    public function languages(): Response
+    {
+
+        return $this->render('front/languages.html.twig');
+    }
+
+    #[Route('/prices', name: 'prices')]
+    public function prices(): Response
+    {
+
+        return $this->render('front/prices.html.twig');
+    }
+
+    #[Route('/tips', name: 'tips')]
+    public function tips(): Response
+    {
+
+        return $this->render('front/tips.html.twig');
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+
+        return $this->render('front/about.html.twig');
+    }
+
+    #[Route('/faq', name: 'faq')]
+    public function faq(): Response
+    {
+
+        return $this->render('front/faq.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+
+        return $this->render('front/contact.html.twig');
+    }
 }
